da3c4e296b661133360f247f17474f2a32d1cfe9
Makarov <infernochetac31@gmail.com>
2023-08-15 20:01:47 +0300

    start hw

---
+++ Makarov/src/test/java/MokitoSpring/Makarov/MakarovApplicationTests.java
@@ -0,0 +1,13 @@
+package MokitoSpring.Makarov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakarovApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
